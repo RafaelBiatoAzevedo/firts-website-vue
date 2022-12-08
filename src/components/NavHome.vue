e7ec22af6da8962751be88bab273aa2f007d3ba6
RafaelBiatoAzevedo <rafaelazevedo321@gmail.com>
2022-12-08 15:44:28 -0300

    feat: add archive

---
+++ src/components/NavHome.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <a>{{ title }}</a>
+</template>
+
+<style scoped>
+a {
+  align-items: center;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  font-size: 1.4rem;
+  font-weight: bold;
+  color: gray;
+  padding: 2.5rem 0;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+a:hover {
+  color: #000;
+  background-color: gray;
+
+  transition: background-color 1000ms linear;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HeaderHome from "../components/HeaderHome.vue";
+import StartHome from "../components/StartHome.vue";
+</script>
+
+<template>
+  <div>
+    <HeaderHome />
+    <StartHome />
+  </div>
+</template>
+
+<style></style>
