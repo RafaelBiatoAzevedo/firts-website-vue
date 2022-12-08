1490b12fe630557569593fec200fcec9fde83c1b
RafaelBiatoAzevedo <rafaelazevedo321@gmail.com>
2022-12-08 14:37:39 -0300

    feat: add component modal whatsapp

---
+++ src/components/ModalWhatsapp.vue
@@ -0,0 +1,43 @@
+<script></script>
+
+<template>
+  <div class="wrapper-main">
+    <div class="wrapper-message">
+      <p>Ficou com alguma dúvida ?</p>
+      <p>Fale com gente</p>
+    </div>
+    <div class="wrapper-icon"></div>
+  </div>
+</template>
+
+<style scoped>
+.wrapper-main {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+.wrapper-message {
+  border-radius: 4px;
+  flex-direction: column;
+  background-color: #fff;
+  padding: 0.6rem 2rem 0.6rem 1rem;
+
+  gap: 5px;
+}
+
+p {
+  font-weight: 500;
+}
+
+.wrapper-icon {
+  position: absolute;
+  background-color: #25d366;
+  border-radius: 100%;
+
+  width: 5rem;
+  height: 5rem;
+  right: -50px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,24 @@
 import HeaderHome from "../components/HeaderHome.vue";
 import StartHome from "../components/StartHome.vue";
 import FooterHome from "../components/FooterHome.vue";
+import ModalWhatsapp from "../components/ModalWhatsapp.vue";
 </script>
 
 <template>
-  <div>
+  <div class="wrapper-main">
     <HeaderHome />
     <StartHome />
     <FooterHome />
+    <div class="wrapper-fixed">
+      <ModalWhatsapp />
+    </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.wrapper-fixed {
+  position: fixed;
+  bottom: 30px;
+  right: 80px;
+}
+</style>
